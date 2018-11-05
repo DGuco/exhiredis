c49452079899f7c5f2354e56469edeb668699e68
dguco <1139140929@qq.com>
2018-11-05 18:12:13 +0800

    bug fix

--- include/exhiredis/config/base_config.hpp
+++ include/exhiredis/config/base_config.hpp
@@ -9,143 +9,144 @@
 #include <string>
 #include <utility>
 
+using namespace std;
+
 namespace exhiredis
 {
-	class CBaseConfig
-	{
-	public:
-		CBaseConfig(string sPassword,
-					string sClientName,
-					int iPingConnectionInterval,
-					bool bKeepAlive,
-					bool bTcpNoDelay)
-			: m_sPassword(std::move(sPassword)),
-			  m_sClientName(std::move(sClientName)),
-			  m_iPingConnectionInterval(iPingConnectionInterval),
-			  m_bKeepAlive(bKeepAlive),
-			  m_bTcpNoDelay(bTcpNoDelay),
-			  m_iIdleConnectionTimeout(10000),
-			  m_iPingTimeout(1000),
-			  m_iConnectTimeout(10000),
-			  m_iTimeout(3000),
-			  m_iRetryAttempts(3),
-			  m_iRetryInterval(1500),
-			  m_iSubscriptionsPerConnection(5)
-		{}
+    class CBaseConfig {
+     public:
+      CBaseConfig (string sPassword,
+                   string sClientName,
+                   int iPingConnectionInterval,
+                   bool bKeepAlive,
+                   bool bTcpNoDelay)
+          : m_sPassword (std::move (sPassword)),
+            m_sClientName (std::move (sClientName)),
+            m_iPingConnectionInterval (iPingConnectionInterval),
+            m_bKeepAlive (bKeepAlive),
+            m_bTcpNoDelay (bTcpNoDelay),
+            m_iIdleConnectionTimeout (10000),
+            m_iPingTimeout (1000),
+            m_iConnectTimeout (10000),
+            m_iTimeout (3000),
+            m_iRetryAttempts (3),
+            m_iRetryInterval (1500),
+            m_iSubscriptionsPerConnection (5)
+      {}
 
-		const string &GetPassword() const
-		{
-			return m_sPassword;
-		}
-		const string &GetClientName() const
-		{
-			return m_sClientName;
-		}
-		int GetPingConnectionInterval() const
-		{
-			return m_iPingConnectionInterval;
-		}
-		bool IsKeepAlive() const
-		{
-			return m_bKeepAlive;
-		}
-		bool IsTtcpNoDelay() const
-		{
-			return m_bTcpNoDelay;
-		}
-		int GetIdleConnectionTimeout() const
-		{
-			return m_iIdleConnectionTimeout;
-		}
-		int GetPingTimeout() const
-		{
-			return m_iPingTimeout;
-		}
-		int GetConnectTimeout() const
-		{
-			return m_iConnectTimeout;
-		}
-		int GetTimeout() const
-		{
-			return m_iTimeout;
-		}
-		int GetRetryAttempts() const
-		{
-			return m_iRetryAttempts;
-		}
-		int GetRetryInterval() const
-		{
-			return m_iRetryInterval;
-		}
-		int GetSubscriptionsPerConnection() const
-		{
-			return m_iSubscriptionsPerConnection;
-		}
+      const string &GetPassword () const
+      {
+        return m_sPassword;
+      }
+      const string &GetClientName () const
+      {
+        return m_sClientName;
+      }
+      int GetPingConnectionInterval () const
+      {
+        return m_iPingConnectionInterval;
+      }
+      bool IsKeepAlive () const
+      {
+        return m_bKeepAlive;
+      }
+      bool IsTtcpNoDelay () const
+      {
+        return m_bTcpNoDelay;
+      }
+      int GetIdleConnectionTimeout () const
+      {
+        return m_iIdleConnectionTimeout;
+      }
+      int GetPingTimeout () const
+      {
+        return m_iPingTimeout;
+      }
+      int GetConnectTimeout () const
+      {
+        return m_iConnectTimeout;
+      }
+      int GetTimeout () const
+      {
+        return m_iTimeout;
+      }
+      int GetRetryAttempts () const
+      {
+        return m_iRetryAttempts;
+      }
+      int GetRetryInterval () const
+      {
+        return m_iRetryInterval;
+      }
+      int GetSubscriptionsPerConnection () const
+      {
+        return m_iSubscriptionsPerConnection;
+      }
 
-		void SetPassword(const string &sPassword)
-		{
-			m_sPassword = sPassword;
-		}
+      void SetPassword (const string &sPassword)
+      {
+        m_sPassword = sPassword;
+      }
 
-		void SetClientName(const string &sClientName)
-		{
-			m_sClientName = sClientName;
-		}
-		void SetPingConnectionInterval(int iPingConnectionInterval)
-		{
-			m_iPingConnectionInterval = iPingConnectionInterval;
-		}
-		void SetKeepAlive(bool bKeepAlive)
-		{
-			m_bKeepAlive = bKeepAlive;
-		}
-		void SetTtcpNoDelay(bool bTtcpNoDelay)
-		{
-			m_bTcpNoDelay = bTtcpNoDelay;
-		}
-		void SetIdleConnectionTimeout(int iIdleConnectionTimeout)
-		{
-			m_iIdleConnectionTimeout = iIdleConnectionTimeout;
-		}
-		void SetPingTimeout(int iPingTimeout)
-		{
-			m_iPingTimeout = iPingTimeout;
-		}
-		void SetConnectTimeout(int iConnectTimeout)
-		{
-			m_iConnectTimeout = iConnectTimeout;
-		}
-		void SetTimeout(int iTimeout)
-		{
-			m_iTimeout = iTimeout;
-		}
-		void SetRetryAttempts(int iRetryAttempts)
-		{
-			m_iRetryAttempts = iRetryAttempts;
-		}
-		void SetRetryInterval(int iRetryInterval)
-		{
-			m_iRetryInterval = iRetryInterval;
-		}
-		void SetSubscriptionsPerConnection(int iSubscriptionsPerConnection)
-		{
-			m_iSubscriptionsPerConnection = iSubscriptionsPerConnection;
-		}
+      void SetClientName (const string &sClientName)
+      {
+        m_sClientName = sClientName;
+      }
+      void SetPingConnectionInterval (int iPingConnectionInterval)
+      {
+        m_iPingConnectionInterval = iPingConnectionInterval;
+      }
+      void SetKeepAlive (bool bKeepAlive)
+      {
+        m_bKeepAlive = bKeepAlive;
+      }
+      void SetTtcpNoDelay (bool bTtcpNoDelay)
+      {
+        m_bTcpNoDelay = bTtcpNoDelay;
+      }
+      void SetIdleConnectionTimeout (int iIdleConnectionTimeout)
+      {
+        m_iIdleConnectionTimeout = iIdleConnectionTimeout;
+      }
+      void SetPingTimeout (int iPingTimeout)
+      {
+        m_iPingTimeout = iPingTimeout;
+      }
+      void SetConnectTimeout (int iConnectTimeout)
+      {
+        m_iConnectTimeout = iConnectTimeout;
+      }
+      void SetTimeout (int iTimeout)
+      {
+        m_iTimeout = iTimeout;
+      }
+      void SetRetryAttempts (int iRetryAttempts)
+      {
+        m_iRetryAttempts = iRetryAttempts;
+      }
+      void SetRetryInterval (int iRetryInterval)
+      {
+        m_iRetryInterval = iRetryInterval;
+      }
+      void SetSubscriptionsPerConnection (int iSubscriptionsPerConnection)
+      {
+        m_iSubscriptionsPerConnection = iSubscriptionsPerConnection;
+      }
 
-	private:
-		string m_sPassword;
-		string m_sClientName;
-		int m_iPingConnectionInterval;
-		bool m_bKeepAlive;
-		bool m_bTcpNoDelay;
-		int m_iIdleConnectionTimeout;
-		int m_iPingTimeout;
-		int m_iConnectTimeout;
-		int m_iTimeout;
-		int m_iRetryAttempts;
-		int m_iRetryInterval;
-		int m_iSubscriptionsPerConnection;
-	};
+     private:
+      string m_sPassword;
+      string m_sClientName;
+      int m_iPingConnectionInterval;
+      bool m_bKeepAlive;
+      bool m_bTcpNoDelay;
+      int m_iIdleConnectionTimeout;
+      int m_iPingTimeout;
+      int m_iConnectTimeout;
+      int m_iTimeout;
+      int m_iRetryAttempts;
+      int m_iRetryInterval;
+      int m_iSubscriptionsPerConnection;
+    };
 }
 
 #endif //EXHIREDIS_BASE_CONFIG_HPP
--- include/exhiredis/connection_pool.hpp
+++ include/exhiredis/connection_pool.hpp
@@ -6,12 +6,56 @@
 #ifndef EXHIREDIS_CONNECTION_POOL_HPP
 #define EXHIREDIS_CONNECTION_POOL_HPP
 
+#include <string>
+#include <list>
+#include "redis_connection.hpp"
+#include "connection_manager.hpp"
+
+using namespace std;
+
 namespace exhiredis
 {
-	class CConnectionPool
-	{
+    class CConnectionPool {
+     public:
+      //construct
+      CConnectionPool (const shared_ptr<IConnectionManager> pConnManager);
+      //add one connection
+      void AddConnection (shared_ptr<CRedisConnection> conn);
+      //borrow one connection
+      shared_ptr<CRedisConnection> BorrowOneConnection ();
+      //back one connection
+      shared_ptr<CRedisConnection> BackOneConnection (shared_ptr<CRedisConnection> conn);
+     private:
+      list<shared_ptr<CRedisConnection>> m_connList;
+      weak_ptr<IConnectionManager> m_pConnManager;
+    };
 
-	};
+    CConnectionPool::CConnectionPool (const shared_ptr<IConnectionManager> pConnManager)
+        : m_pConnManager (pConnManager)
+    {
+      m_connList.clear ();
+    }
+
+    void CConnectionPool::AddConnection (shared_ptr<CRedisConnection> conn)
+    {
+      m_connList.push_back (std::move (conn));
+    }
+
+    shared_ptr<CRedisConnection> CConnectionPool::BorrowOneConnection ()
+    {
+      if (m_connList.size () > 0)
+        {
+          shared_ptr<CRedisConnection> &it = m_connList.front ();
+          m_connList.pop_front ();
+          return it;
+        }
+      return nullptr;
+    }
+
+    shared_ptr<CRedisConnection> CConnectionPool::BackOneConnection (shared_ptr<CRedisConnection> conn)
+    {
+      m_connList.push_back (std::move (conn));
+    }
 }
 
 #endif //EXHIREDIS_CONNECTION_POOL_HPP
