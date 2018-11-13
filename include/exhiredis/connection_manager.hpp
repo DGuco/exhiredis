37f637264c1ff5b1019fe000e43b83790e54ea8f
dguco <1139140929@qq.com>
2018-11-13 19:10:59 +0800

    crc

--- include/exhiredis/connection_manager.hpp
+++ include/exhiredis/connection_manager.hpp
@@ -6,17 +6,31 @@
 #define EXHIREDIS_CONNECT_MANAGER_H
 
 #include <memory>
-#include "config/redis_config.hpp"
-
+#include <string>
 using namespace std;
+
+class CMasterSlaveEntry;
+class MasterSlaveServersConfig;
+class CRedisConfig;
 
 namespace exhiredis
 {
 class IConnectionManager
 {
 public:
+    //calculate the hash slot of the key
+    virtual int CalcSlot(string key) = 0;
+    //calculate the hash slot of the key
+    virtual int CalcSlot(char *key, int len) = 0;
+    //get config
+    virtual shared_ptr<MasterSlaveServersConfig> GetConfig() = 0;
+    //get the redis entry of the hash slot
+    virtual shared_ptr<CMasterSlaveEntry> GetEntry(int slot) = 0;
+    //shutdown
     virtual void ShutDown() = 0;
-    virtual bool IsShubDown() = 0;
+    //is shutdown
+    virtual bool IsShutDown() = 0;
+    //get config
     virtual shared_ptr<CRedisConfig> &GetRedisConfig() = 0;
 };
 }
--- include/exhiredis/master_slave_manager.hpp
+++ include/exhiredis/master_slave_manager.hpp
@@ -16,6 +16,13 @@ class CMasterSlaveManager: public IConnectionManager
 {
 public:
     CMasterSlaveManager(const shared_ptr<CRedisConfig> &m_pRedisConfig);
+    int CalcSlot(string key) override;
+    int CalcSlot(char *key, int len) override;
+    shared_ptr<MasterSlaveServersConfig> GetConfig() override;
+    shared_ptr<CMasterSlaveEntry> GetEntry(int slot) override;
+    void ShutDown() override;
+    bool IsShutDown() override;
+    shared_ptr<CRedisConfig> &GetRedisConfig() override;
 private:
     shared_ptr<CRedisConfig> m_pRedisConfig;
 };
@@ -24,6 +31,41 @@ CMasterSlaveManager::CMasterSlaveManager(const shared_ptr<CRedisConfig> &m_pRedi
     : m_pRedisConfig(m_pRedisConfig)
 {
 
+}
+
+int CMasterSlaveManager::CalcSlot(string key)
+{
+    return 0;
+}
+
+int CMasterSlaveManager::CalcSlot(char *key, int len)
+{
+    return 0;
+}
+
+shared_ptr<MasterSlaveServersConfig> CMasterSlaveManager::GetConfig()
+{
+    return nullptr;
+}
+
+shared_ptr<CMasterSlaveEntry> CMasterSlaveManager::GetEntry(int slot)
+{
+    return nullptr;
+}
+
+void CMasterSlaveManager::ShutDown()
+{
+
+}
+
+bool CMasterSlaveManager::IsShutDown()
+{
+    return false;
+}
+
+shared_ptr<CRedisConfig> &CMasterSlaveManager::GetRedisConfig()
+{
+    return <#initializer#>;
 }
 }
 #endif //EXHIREDIS_MASTERSLAVEMANAGER_HPP
